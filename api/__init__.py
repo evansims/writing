db76f4ff44e019c8a3e6b84c6e8806feca049235
Evan Sims <hello@evansims.com>
2025-03-29 01:07:17 -0500

    api: fixes

--- api/__init__.py
+++ api/__init__.py
@@ -1,9 +1,1 @@
-import os
-import sys
-
-sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-
-from .content import content_bp
-from .audio import audio_bp
-
-__all__ = ["content_bp", "audio_bp"]
+# This file is intentionally kept blank to mark the 'api' directory as a Python package.
